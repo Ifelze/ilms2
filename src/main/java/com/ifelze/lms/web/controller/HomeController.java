b534dad7f76c6b2c2f36afb752ae433679ee4087
nagaifelze <naga@ifelze.com>
2016-11-27 21:52:33 -0500

    Added Terms * conditions

--- src/main/java/com/ifelze/lms/web/controller/HomeController.java
+++ src/main/java/com/ifelze/lms/web/controller/HomeController.java
@@ -5,10 +5,7 @@ package com.ifelze.lms.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.ifelze.lms.web.rest.vm.ManagedUserVM;
 
 /**
  * @author nithya
@@ -19,5 +16,10 @@ public class HomeController {
 	@RequestMapping("start")
     public String getIndex(Model model){
         return "start";
+    }
+    
+    @RequestMapping("terms_conditions")
+    public String getTermsConditions(Model model){
+        return "terms_conditions";
     }
 }
